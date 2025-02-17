d5b88a5aaaf83a209b06e81a765e48058dac3e78
OlaOdejimi <odejimiolasunkanmi@gmail.com>
2025-02-17 12:05:07 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+// This script handles basic interactions like smooth scrolling, menu toggle, etc.
+document.addEventListener('DOMContentLoaded', () => {
+    // You can add any interactivity here for things like buttons or product carousels
+});
